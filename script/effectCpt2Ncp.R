60f589eb99f608610a36476aff14a8b9c45ef617
yuchenw2015 <37629284+yuchenw2015@users.noreply.github.com>
2021-06-16 08:20:33 -0500

    Update effectCpt2Ncp.R

--- script/effectCpt2Ncp.R
+++ script/effectCpt2Ncp.R
@@ -16,9 +16,10 @@ gc()
 modelName <- "effectCpt2Ncp"
 simModelName <- "effectCpt2NcpSim"
 
-## Relative paths assuming the working directory is the Stan_and_Torsten directory
+## Relative paths assuming the working directory is the script directory
+## containing this script
 scriptDir <- getwd()
-projectDir <- scriptDir
+projectDir <- dirname(scriptDir)
 figDir <- file.path(projectDir, "deliv", "figure", modelName)
 tabDir <- file.path(projectDir, "deliv", "table", modelName)
 dataDir <- file.path(projectDir, "data", "derived")
